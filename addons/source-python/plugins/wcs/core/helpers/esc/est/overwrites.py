92304ab29ef8255466a90e803a22206a2ce610ed
ThaPwned <thapwned@hotmail.com>
2019-07-12 14:33:06 +0200

    Forced the est_* to be all lower-case instead of mixing them
    This is only temporary until a solution has been found

--- addons/source-python/plugins/wcs/core/helpers/esc/est/overwrites.py
+++ addons/source-python/plugins/wcs/core/helpers/esc/est/overwrites.py
@@ -22,26 +22,14 @@ __all__ = (
 class ESTCommand(_TypedServerCommand):
     def __init__(self, commands, permission=None, fail_callback=None):
         if isinstance(commands, list):
+            commands[0] = commands[0].lower()
+
             if not commands[0].startswith('est_'):
                 commands[0] = f'est_{commands[0]}'
         else:
+            commands = commands.lower()
+
             if not commands.startswith('est_'):
                 commands = f'est_{commands}'
 
         super().__init__(commands, permission, fail_callback)
-
-    def __call__(self, callback):
-        super().__call__(callback)
-
-        commands = self.commands
-
-        if not (commands[0] if isinstance(commands, list) else commands).islower():
-            if isinstance(commands, list):
-                commands = [*commands]
-                commands[0] = commands[0].lower()
-            else:
-                commands = commands.lower()
-
-            ESTCommand(commands, self.permission, self.fail_callback)(callback)
-
-        return callback
