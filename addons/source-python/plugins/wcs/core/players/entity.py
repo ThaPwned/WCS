2ade0cce5229b55291eb2112010178dbf40fd9bd
ThaPwned <thapwned@hotmail.com>
2021-03-30 18:54:33 +0200

    Added a temporary solution to prevent an infinite loop
    This stops WCS from inserting a player's data more than once, so it won't go into an infinite loop. The player who triggered it might be in an invalid state, and will have to reconnect to solve it

--- addons/source-python/plugins/wcs/core/players/entity.py
+++ addons/source-python/plugins/wcs/core/players/entity.py
@@ -280,6 +280,7 @@ class Player(object, metaclass=_PlayerMeta):
         self._stats = _Stats()
         self._ready = False
         self._retrieving = False
+        self._inserting = False
         self._privileges = {}
 
         self._id = None
@@ -312,8 +313,14 @@ class Player(object, metaclass=_PlayerMeta):
         data = result.fetchone()
 
         if data is None:
+            if self._inserting:
+                raise RuntimeError(f'Tried to insert a player\'s data more than once ({self.accountid} - {result["name"]}')
+
+            self._inserting = True
+
             database_manager.execute('player insert', (None if isinstance(self.accountid, str) else self.accountid, result['name'], race_manager.default_race, time(), cfg_new_player_bank_bonus.get_int(), 0))
             database_manager.execute('player get' + (' bot' if isinstance(self.accountid, str) else ''), (self.accountid, ), callback=self._query_get_player, name=result['name'])
+
             return
 
         self._id = data[0]
